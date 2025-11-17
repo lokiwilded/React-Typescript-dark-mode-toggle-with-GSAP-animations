9fcf3ba89b5cdad4fb6006c753c6e0a5f23582c2
lokiwilded <lokidenicolo1702@gmail.com>
2025-11-17 12:47:46 +0000

    fix: Correct base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 
 export default defineConfig({
-  base: '/React-Typescript-dark-mode-toggle-with-GSAP-animations-/',
+  base: '/React-Typescript-dark-mode-toggle-with-GSAP-animations/',
   plugins: [
     react(),
   ],
